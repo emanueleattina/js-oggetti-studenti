32a32ee1be954d212078d7605646ef1ac2863f32
emanuele <emattina97@gmail.com>
2021-03-17 09:40:33 +0100

    Carico dettaglio grafico

--- main.js
+++ main.js
@@ -37,6 +37,9 @@ $(document).ready(function () {
             
             $('#classe').append("<h2>" + (i + 1) + "° studente: " + classe[i].nome + ' ' + classe[i].cognome + "</h2>");
         }
+
+        $('#classe').children().addClass('mt-20')
+
         
     // l'utente deve poter aggiungere un nuovo oggetto studente
         // inserire tramite form nome, cognome ed età
@@ -63,6 +66,8 @@ $(document).ready(function () {
         for(i = 0; i < classe.length; i++) {
             // console.log((i + 1) + "° studente: " + classe[i].nome + ' ' + classe[i].cognome);
             $('#classe').append("<h2>" + (i + 1) + "° studente: " + classe[i].nome + ' ' + classe[i].cognome + "</h2>");
+            $('#classe').children().addClass('mt-20')
+
         }
 
 
